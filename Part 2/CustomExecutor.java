30b267c902b931c745143c6eedd007976edd9ad3
Yehonatan Mekayten <99586548+yehonatan768@users.noreply.github.com>
2023-01-08 19:54:36 +0200

    Update CustomExecutor.java

--- Part 2/CustomExecutor.java
+++ Part 2/CustomExecutor.java
@@ -1,11 +1,23 @@
 import java.util.concurrent.*;
 
+/**
+This class represents a custom thread pool executor that is capable of executing tasks with different priorities.
+The number of threads in the pool is set to half the number of processors available for the JVM. The maximum number of threads is set to the number of processors minus 1.
+The threads in the pool will remain alive for a maximum idle time of 300 milliseconds (0.3 seconds).
+A priority blocking queue is used to store the tasks and their priorities.
+@author Noy Dayan, Yehonatan Mekayten.
+@version 1.
+*/
 public class CustomExecutor {
     final long MAXIDLETIME = 300; // section 9, 300 millisecond (0.3 seconds)
     private final PriorityBlockingQueue<Runnable> threadPool; // section 7
     private final ThreadPoolExecutor executor;
     private int maxPriority;
 
+    /**
+    Constructor for the CustomExecutor class.
+    Initializes the thread pool and the thread pool executor with the specified parameters.
+    */
     public CustomExecutor() {
         int availableProcessors = Runtime.getRuntime().availableProcessors();
         int minProcessors =  availableProcessors / 2; // section 5
@@ -15,12 +27,23 @@ public class CustomExecutor {
         maxPriority = Integer.MIN_VALUE;
     }
 
+    /**
+    Submits a task with a specified priority to the thread pool executor.
+    @param task The task to be executed
+    @return A Future object representing the result of the task
+    */
     // section 1
     public <T> Future<T> submit(Task<T> task) {
         maxPriority = Math.max(maxPriority, task.getPriority());
         return executor.submit(task);
     }
-
+    
+    /**
+    Creates a task from a callable and a specified priority, and submits it to the thread pool executor.
+    @param callable The callable representing the task to be executed
+    @param type The priority of the task
+    @return A Future object representing the result of the task
+    */
     // section 2
     public <T> Future<T> submit(Callable<T> callable, TaskType type) {
         Task<T> task = new Task<>(callable, type);
@@ -28,6 +51,12 @@ public class CustomExecutor {
         return this.submit(task); // section 4
     }
 
+    /**
+    Creates a task from a callable and submits it to the thread pool executor.
+    The priority of the task is set to the default value.
+    @param callable The callable representing the task to be executed.
+    @return A Future object representing the result of the task.
+    */
     // section 3
     public <T> Future<T> submit(Callable<T> callable) {
         Task<T> task = new Task<T>(Task.createTask(callable));
@@ -35,15 +64,27 @@ public class CustomExecutor {
         return this.submit(task); // section 4
     }
 
+    /**
+    Returns the maximum priority of any task submitted to this executor.
+    @return The maximum priority of any task submitted to this executor.
+    */
     public int getMaxPriority() {
         return this.maxPriority;
     }
-
+    
+    /**
+    Returns the current maximum priority of any task in the queue.
+    @return The current maximum priority of any task in the queue.
+    */
     // section 10
     public int getCurrentMax() {
         return maxPriority;
     }
-
+    
+    /**
+    Shuts down the executor in a graceful manner.
+    Allows currently executing tasks to complete, but no new tasks will be accepted.
+    */
     // section 11
     public void gracefullyTerminate() {
         executor.shutdown();
