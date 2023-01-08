163ab428f3fd7aeeccc789666291be301910f1fe
Yehonatan Mekayten <99586548+yehonatan768@users.noreply.github.com>
2023-01-08 17:12:10 +0200

    Update FileLineCounterThread.java

--- Part 1/FileLineCounterThread.java
+++ Part 1/FileLineCounterThread.java
@@ -2,13 +2,29 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * A class that extends the Thread class to count the number of lines in a text file.
+ *
+ * @author  [Your Name]
+ * @version [Insert Version Number]
+ * @since   [Insert Date]
+ */
 public class FileLineCounterThread extends Thread {
     private String fileName;
     private int numLines;
-
+    
+    /**
+ * Constructs a FileLineCounterThread object.
+ *
+ * @param fileName The name of the file to count the lines of.
+ */
     public FileLineCounterThread(String fileName) {
         this.fileName = fileName;
     }
+    
+    /**
+ * Counts the number of lines in a text file.
+ */
 
     @Override
     public void run() {
@@ -21,7 +37,12 @@ public class FileLineCounterThread extends Thread {
             e.printStackTrace();
         }
     }
-
+    
+    /**
+ * Returns the number of lines in the file.
+ *
+ * @return The number of lines in the file.
+ */
     public int getNumLines() {
         return numLines;
     }
