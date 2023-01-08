c4c8acbb23c8b6e0e5ce9110a09cb747b8f66594
Yehonatan Mekayten <99586548+yehonatan768@users.noreply.github.com>
2023-01-08 17:15:22 +0200

    Update EX2.java

--- Part 1/EX2.java
+++ Part 1/EX2.java
@@ -1,8 +1,20 @@
 import java.io.IOException;
 
-
+/**
+ * A class for comparing the performance of three functions that count the number of lines in a set of text files.
+ * The first function uses a single thread, the second uses multiple threads, and the third uses a thread pool.
+ *
+ * @author  [Your Name]
+ * @version [Insert Version Number]
+ * @since   [Insert Date]
+ */
 public class EX2 {
-
+    
+    /**
+ * Calls the 'getNumOfLines' function in EX2_1 and measures the time taken to execute the function.
+ *
+ * @param fileNames An array of file names.
+ */
     public static void activateFunction1(String[] fileNames) {
         try {
             long startTime = System.nanoTime();
@@ -14,7 +26,12 @@ public class EX2 {
             System.out.println("[Exception] An error occurred in function 'getNumOfLines'");
         }
     }
-
+    
+    /**
+ * Calls the 'getNumOfLinesThreads' function in EX2_1 and measures the time taken to execute the function.
+ *
+ * @param fileNames An array of file names.
+ */
     public static void activateFunction2(String[] fileNames) {
         try {
             long startTime = System.nanoTime();
@@ -26,7 +43,12 @@ public class EX2 {
             System.out.println("[Exception] An error occurred in function 'getNumOfLinesThreads'");
         }
     }
-
+    
+    /**
+ * Calls the 'getNumOfLinesThreadPool' function in EX2_1 and measures the time taken to execute the function.
+ *
+ * @param fileNames An array of file names.
+ */
     public static void activateFunction3(String[] fileNames) {
         try {
             long startTime = System.nanoTime();
@@ -38,7 +60,15 @@ public class EX2 {
             System.out.println("[Exception] An error occurred in function 'getNumOfLinesThreadPool'");
         }
     }
-
+    
+    /**
+ * The main method of the EX2 class. Creates a specified number of text files, calls the three functions that
+ * count the number of lines in the files, and measures the time taken to execute each function. The main method
+ * also deletes the created text files.
+ *
+ * @param args Command line arguments (not used).
+ * @throws IOException If an error occurs while creating or deleting the text files.
+ */
     public static void main(String[] args) throws IOException {
         int N = 1000, SEED = 22321, BOUND = 3000;
         String[] fileNames = EX2_1.createTextFiles(N,SEED,BOUND);
