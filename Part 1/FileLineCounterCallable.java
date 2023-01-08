827a729b53751594353a941ff596cfbbf018548e
Yehonatan Mekayten <99586548+yehonatan768@users.noreply.github.com>
2023-01-08 17:10:22 +0200

    Update FileLineCounterCallable.java

--- Part 1/FileLineCounterCallable.java
+++ Part 1/FileLineCounterCallable.java
@@ -3,14 +3,31 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.concurrent.Callable;
 
-
+/**
+ * A class that implements the Callable interface to count the number of lines in a text file.
+ *
+ * @author  [Your Name]
+ * @version [Insert Version Number]
+ * @since   [Insert Date]
+ */
 public class FileLineCounterCallable implements Callable<Integer> {
     private String fileName;
-
+    
+    /**
+ * Constructs a FileLineCounterCallable object.
+ *
+ * @param fileName The name of the file to count the lines of.
+ */
     public FileLineCounterCallable(String fileName) {
         this.fileName = fileName;
     }
-
+    
+    /**
+ * Counts the number of lines in a text file.
+ *
+ * @return The number of lines in the file.
+ * @throws Exception If an error occurs while reading the file.
+ */
     @Override
     public Integer call() {
         int numLines = 0;
